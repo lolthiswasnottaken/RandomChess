a4fa3d6d24f4e7ff7647fb95c08dc2d1dd475ec2
lolthiswasnottaken <82940334+lolthiswasnottaken@users.noreply.github.com>
2021-05-30 13:00:15 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -13,12 +13,28 @@ import time
 class Game():
     def __init__(self):
         self.create_board()
+        #self.display_board()
+        self.legal_moves()
+
+        self.make_first_move()
         self.display_board()
-    
+
     def create_board(self):
         self.board = chess.Board()
     
     def display_board(self):
         print(self.board)
+
+    def legal_moves(self):
+        self.legal_moves = [str(i) for i in (self.board.legal_moves)]
+    
+    def decide_move(self):
+        return random.randint(0,len(self.legal_moves)-1)
+
+    def make_first_move(self):
+        print(self.legal_moves[self.decide_move()])
+        self.board.push_san(self.legal_moves[self.decide_move()])
+        
+
 
 g = Game()
