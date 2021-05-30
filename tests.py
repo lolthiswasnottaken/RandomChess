0c08977d8bdefae3c2a70761cf47fb35b89e3389
lolthiswasnottaken <82940334+lolthiswasnottaken@users.noreply.github.com>
2021-05-30 13:21:40 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,40 +1,49 @@
 #!/usr/bin/env python3
 
 #tests.py
-
-
 import chess
 import chess.svg
 from IPython.display import SVG
 import random
 import time
 
-
-class Game():
-    def __init__(self):
-        self.create_board()
-        #self.display_board()
-        self.legal_moves()
-
-        self.make_first_move()
-        self.display_board()
-
-    def create_board(self):
-        self.board = chess.Board()
-    
-    def display_board(self):
-        print(self.board)
-
-    def legal_moves(self):
-        self.legal_moves = [str(i) for i in (self.board.legal_moves)]
-    
-    def decide_move(self):
-        return random.randint(0,len(self.legal_moves)-1)
-
-    def make_first_move(self):
-        print(self.legal_moves[self.decide_move()])
-        self.board.push_san(self.legal_moves[self.decide_move()])
+board = chess.Board()
+game_pgn = []
+#legal_moves = [str(i) for i in list(board.legal_moves)]
+legal_moves= [1]
+moves = 1
+game_num= 0
+while 1:
+  
         
+        legal_moves = [str(i) for i in list(board.legal_moves)]
+        move_ind = random.randint(0,len(legal_moves)-1)
+        board.push_san(legal_moves[move_ind])
+        game_pgn.append("{}.{}".format(int(moves),legal_moves[move_ind]))
+        
+        #time.sleep(1)
 
 
-g = Game()
+        #print(board.board_fen)
+        #print(game_pgn)
+        print(board,"\n")
+        #print(board.fen)
+        moves += 0.5
+    
+                
+        if board.is_game_over():
+
+            if board.is_checkmate():
+                pass
+                #boardsvg = chess.svg.board(board=board)
+                #outputfile = open('board_images/game#{}.svg'.format(game_num), "w")
+                #outputfile.write(boardsvg)
+                #outputfile.close()
+
+            board.clear
+            board = chess.Board()
+            game_pgn = []
+            moves = 1
+            game_num += 1   
+                
+    
